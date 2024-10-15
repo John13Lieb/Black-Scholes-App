431bb306c1252d77167013f67823642159196230
John Lieb <144958513+John13Lieb@users.noreply.github.com>
2024-10-15 16:39:40 -0500

    Merge option price methods
    Combined call_price with put_price and added new method get_intermediates to calculate d1 and d2

--- functions.py
+++ functions.py
@@ -1,22 +1,18 @@
 from math import exp, sqrt, log
 from scipy.stats import norm
 
-# calculates the call option price
-def call_price(S, K, sigma, r, T):
-    if S > 0 and K > 0 and sigma >= 0 and T > 0:
-        d1 = (log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * sqrt(T))
-        d2 = d1 - sigma * sqrt(T)
-        call = S * norm.cdf(d1) - K * exp(-r * T) * norm.cdf(d2)
-        return call
-    else:
-        return None
+# calculates the intermediate values d1 and d2
+def get_intermediates(S, K, sigma, r, T):
+    d1 = (log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * sqrt(T))
+    d2 = d1 - sigma * sqrt(T)
+    return d1, d2
 
-# calculates the put option price
-def put_price(S, K, sigma, r, T):
+# calculates the call and put option prices
+def calc_prices(S, K, sigma, r, T):
     if S > 0 and K > 0 and sigma >= 0 and T > 0:
-        d1 = (log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * sqrt(T))
-        d2 = d1 - sigma * sqrt(T)
+        d1, d2 = get_intermediates(S, K, sigma, r, T)
+        call = S * norm.cdf(d1) - K * exp(-r * T) * norm.cdf(d2)
         put = K * exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
-        return put
+        return call, put
     else:
         return None
